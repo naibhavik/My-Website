a3396a32fafd263571ab8a1028daadfa9dacda94
Nai Bhavik <116361800+naibhavik@users.noreply.github.com>
2023-07-30 09:53:29 +0530

    Add files via upload

---
+++ My Website/my.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".ee", {
+    strings: ['Full Stack Web Developer!', 'Software Developer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
